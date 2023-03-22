6d77a4c5348672e7cb35e59e405e110f8e9f9046
Szymon Kokot <azerty0220pl@gmail.com>
2023-03-22 12:13:54 +0100

    first implementation of the challenge

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
+let urls = [];
+
 app.use(cors({optionsSuccessStatus: 200}));
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.static('public'));
@@ -11,7 +13,21 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.route('/api/shorturl').post((req, res) => {
+  console.log(req.body);
+  urls.push(req.body.url);
+  res.json({"original_url": req.body.url, "short_url": urls.length - 1});
+});
 
+app.get("/api/shorturl/:index", function (req, res) {
+  let x = parseInt(req.params.index);
+  if(x < 0 || isNaN(x) || x >= urls.length)
+  {
+    res.json({"error": "invalid url"});
+  } else {
+    res.redirect(urls[x]);
+  }
+});
 
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
