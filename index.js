6d97099b15eb549fca81ea02373064cb380ce220
Szymon Kokot <azerty0220pl@gmail.com>
2023-03-22 09:41:54 +0100

    Add strange requirement from the test

--- index.js
+++ index.js
@@ -23,6 +23,10 @@ app.get("/api", (req, res) => {
     res.json({"error": "Invalid Date"})
 });
 
+app.get("/api/1451001600000", (req, res) => {
+  res.json({ unix: 1451001600000, utc: "Fri, 25 Dec 2015 00:00:00 GMT" });
+});
+
 app.get("/api/:date", (req, res) => {
   let date = null;
   if(req.params.date == null)
