6033f92d40bb0de503d4268f14d840cfa2ba15c4
Szymon Kokot <azerty0220pl@gmail.com>
2023-03-22 11:51:59 +0100

    previous challenge remove from index.js

--- index.js
+++ index.js
@@ -10,35 +10,7 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-app.get("/api", (req, res) => {
-  let date = null;
-  if(req.query.date == null)
-    date = new Date();
-  else
-    date = new Date(req.query.date);
 
-  if(!isNaN(date.getTime()))
-    res.json({"unix": date.getTime(), "utc": date.toUTCString()});
-  else
-    res.json({"error": "Invalid Date"})
-});
-
-app.get("/api/1451001600000", (req, res) => {
-  res.json({ unix: 1451001600000, utc: "Fri, 25 Dec 2015 00:00:00 GMT" });
-});
-
-app.get("/api/:date", (req, res) => {
-  let date = null;
-  if(req.params.date == null)
-    date = new Date();
-  else
-    date = new Date(req.params.date);
-
-  if(!isNaN(date.getTime()))
-    res.json({"unix": date.getTime(), "utc": date.toUTCString()});
-  else
-    res.json({"error": "Invalid Date"})
-});
 
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
