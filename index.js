b956a1707c55e07786d461b714e1cc690af0451d
Szymon Kokot <azerty0220pl@gmail.com>
2023-03-23 19:18:35 +0100

    database setup

--- index.js
+++ index.js
@@ -6,6 +6,24 @@ const mongoose = require('mongoose');
 
 mongoose.connect(process.env['MONGO_URI'], { useNewUrlParser: true, useUnifiedTopology: true });
 
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  _id: { type: String, required: true },
+  username: { type: String, required: true },
+  count: {type: Number, required: false}
+});
+const User = mongoose.model("User", userSchema);
+
+const exerciseSchema = new Schema({
+  _id: { type: String, required: true },
+  description: { type: String, required: true },
+  duration: { type: Number, required: true },
+  date: { type: String, required: true },
+  user: {type: mongoose.Types.ObjectId, ref: "user"}
+});
+const Exercise = mongoose.model("Exercise", exerciseSchema);
+
 app.use(cors({optionsSuccessStatus: 200}));
 
 app.use(express.static('public'));
