7544290c5d0942f79429b24ba5567979c6835b62
Szymon Kokot <azerty0220pl@gmail.com>
2023-03-24 16:33:08 +0100

    overwrite fix

--- index.js
+++ index.js
@@ -68,7 +68,9 @@ app.post("/api/users/:id/exercises", (req, res) => {
     let ex = new Exercise({_id: doc._id.toString() + '-' + doc.count, description: req.body.description, duration: req.body.duration, date: date, user: doc._id});
     
     console.log({"username": doc.username, "description": ex.description, "duration": ex.duration, date: ex.date, "_id": doc._id});
-    doc.overwrite({_id: doc._id, username: doc.username, count: doc.count + 1});
+
+    doc.overwrite({username: doc.username, count: doc.count + 1});
+    doc.save();
 
     ex.save().then((doc) => {
       console.log("saved");
