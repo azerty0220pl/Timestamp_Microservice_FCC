0f79382c225951eb85400f08de52dad1e1d4f4f2
Szymon Kokot <azerty0220pl@gmail.com>
2023-03-25 12:12:52 +0100

    multer setup

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const multer  = require('multer')
+const upload = multer({ dest: './uploads/' })
 const app = express();
 const cors = require('cors');
 
@@ -9,6 +11,10 @@ app.use(express.static('public'));
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
+
+app.post("/api/fileanalyse", upload.single('file'), (req, res) => {
+  res.json({"name": req.file.originalName});
+})
 
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
