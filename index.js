f9db0de984b19a979a117e03e7e9fff45d10c6dd
Szymon Kokot <azerty0220pl@gmail.com>
2023-04-12 11:42:35 +0200

    secret setup

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(session({
-  secret: process.env.SESSION_SECRET,
+  secret: "process.env.SESSION_SECRET:is a random value",
   resave: true,
   saveUninitialized: true,
   cookie: {
